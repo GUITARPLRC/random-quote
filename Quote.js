43acaf89e5a4cc8794fc68be9993fa8a4e133e68
Charles <GUITARPLRC@YAHOO.COM>
2015-12-28 09:54:34 -0600

    Random quote

---
+++ Quote.js
@@ -0,0 +1,18 @@
+var quotes = ['"The best way to cheer yourself up is to try to cheer somebody else up." - Mark Twain', 
+	'"Confusion is the beginning of wisdom." - Socrates', 
+	'"The difference between a mountain and a molehill is your perspective." - Al Neuharth', 
+	'"Act the part and you will become the part." - William James', 
+	'"You always pass failure on the way to success." - Mickey Rooney', 
+	'"When you have to make a choice and you don\'t make it, that itself is a choice." - William James', 
+	'"How we spend our days is, of course, how we spend our lives." - Annie Dillard', 
+	'"Life moves pretty fast. If you don\'t stop and look around once in a while, you could miss it." - Ferris Bueller', 
+	'"The important thing is not to stop questioning" - Albert Einstein', 
+	'"Enjoy the little things, for one day you may look back and realize they were the big things." - Robert Brault'];
+	
+function chooseQuote() {
+	
+	var number = Math.floor(Math.random() * 9);
+	console.log(quotes[number]);
+}
+
+chooseQuote();
